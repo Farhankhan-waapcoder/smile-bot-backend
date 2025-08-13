dfe8b80414291284fb574b35d2f001d48fcd81dc
farhanmmb2004 <farhankhanmmb2004@gmail.com>
2025-08-13 17:57:07 +0530

    setup done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {ApiResponse} from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(500).json({
-        message: "chai aur code"
-    })
+     res.status(200).json(new ApiResponse(true, "User registered successfully", {}));
 } )
 
 
